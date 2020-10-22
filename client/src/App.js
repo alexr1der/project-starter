eb4e9d512959ad028d7cebfcaa9bc5651e57a87d
Oleksii Hordiienko <myspacealexr1der@gmail.com>
2020-10-22 18:15:32 -0400

    //testing

--- client/src/App.js
+++ client/src/App.js
@@ -10,18 +10,40 @@ import PostsListPage from './pages/PostsListPage';
 import PostFormPage from './pages/PostFormPage';
 import ShowPostPage from './pages/ShowPostPage';
 import AboutUsPage from './pages/AboutUsPage';
+import Mypage from './pages/MyPage';
+import MyPage from './pages/MyFriends';
+import PlaceWizard from './pages/PlaceWizard';
 
 import './App.css';
 
 
 function Navigation(props) {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-      <Link className="navbar-brand" to="/">Micro Blog</Link>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary shadow mb-3">
+      <Link className="navbar-brand" to="/">TripGram</Link>
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
           <NavLink className="nav-link" exact to="/posts/new">
-            Create a Micro Post
+            Create a Post
+          </NavLink>
+        </li>
+         <li className="nav-item">
+          <NavLink className="nav-link" exact to="/MyPage">
+            My Page
+          </NavLink>
+        </li>
+
+      
+         <li className="nav-item">
+          <NavLink className="nav-link" exact to="/MyFriends">
+            My Friends
+          </NavLink>
+        </li>
+
+      
+         <li className="nav-item">
+          <NavLink className="nav-link" exact to="/PlaceWizard">
+            Place Wizard
           </NavLink>
         </li>
         <li className="nav-item">
@@ -45,6 +67,9 @@ class App extends React.Component {
               <Switch>
                 <Route path="/posts/new" component={PostFormPage} />
                 <Route path="/posts/:id" component={ShowPostPage} />
+                 <Route path="/MyPage" component={AboutUsPage} />
+                 <Route path="/MyFriends" component={AboutUsPage} />
+                 <Route path="/PlaceWizard" component={AboutUsPage} />
                 <Route path="/about-us" component={AboutUsPage} />
                 <Route path="/" component={PostsListPage} />
               </Switch>
--- client/src/pages/PostFormPage.js
+++ client/src/pages/PostFormPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import Draggable from "react-draggable";
+import DraggableCore from "react-draggable";
 
 class PostFormPage extends React.Component {
   state = {
     error: false,
     success: false,
     content: '',
+   
   }
 
   contentChanged = (event) => {
@@ -40,9 +43,13 @@ class PostFormPage extends React.Component {
           error: true,
         });
       });
-  }
-
+  
+   //Starts from here
+ 
+ //Ends here
+}
   render() {
+
     if(this.state.success) return <Redirect to="/" />;
 
     let errorMessage = null;
@@ -54,20 +61,60 @@ class PostFormPage extends React.Component {
       );
     }
 
-    return (
+    return (<Draggable
+         handle=".drag"
+         defaultPosition={{x: 0, y: 0}}
+         position={null}
+         grid={[5, 5]}
+         scale={1}
+         onStart={this.handleStart}
+         onDrag={this.handleDrag}
+         onStop={this.handleStop}
+         >
+<div className="drag">
       <div className="col-10 col-md-8 col-lg-7">
         { errorMessage }
-        <div className="input-group">
+   
+         <div id="drop-region">
+    <div class="drop-message">
+        Drag & Drop images or click to upload
+    </div>
+    <div id="image-preview"></div>
+                </div>
+
+
+
+
+
+
+<div id="drop-region">
+    <div class="drop-message">
+        Drag & Drop images or click to upload
+    </div>
+    <div id="image-preview"></div>
+</div>
+
+
+
+
+
+
+        <div className="input-group, drag">
+ 
           <input 
             type="text" 
             placeholder="Add your words of wisdom here..." 
             value={this.state.content}
+            
             className="form-control mr-3 rounded"
             onChange={this.contentChanged}
           />
+
           <button className="btn btn-primary" onClick={this.savePost}>Save Post</button>
         </div>
       </div>
+      </div>
+ </ Draggable>
     );
   }
 }
